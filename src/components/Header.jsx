cff9af4c00973e83a900b2317ff58edcf832e630
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-08-07 18:06:36 +0100

    adds burger menu for small screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,20 @@ import NavBar from './NavBar';
 
 const Header = () => {
     const { colorMode, toggleColorMode } = useColorMode();
+
     return (
-        <Flex mb={5} pb={5} as="header" flexDirection="column" maxW={1350} alignSelf="center" mx="auto" boxShadow="0 1px 3px rgba(0, 0, 0, 0.5)" >
+        <Flex mb={5} pb={5} as="header" flexDirection="column" alignSelf="center" mx="auto" boxShadow="0 1px 3px rgba(0, 0, 0, 0.5)" >
             <Flex mt={5} mx={5} justify="space-between" >
                 <Heading as="h1">Ellen Daly</Heading>
-                <NavBar />
-                <IconButton
-                            ml={2}
-                            onClick={toggleColorMode}
-                            aria-label="Toggle light/dark mode"
-                            icon={colorMode === 'light' ? <FaRegMoon /> : <MdOutlineWbSunny />}
-                        />
+                <Flex alignItems="center" gap={5}>
+                    <NavBar />
+                    <IconButton
+                        ml={2}
+                        onClick={toggleColorMode}
+                        aria-label="Toggle light/dark mode"
+                        icon={colorMode === 'light' ? <FaRegMoon /> : <MdOutlineWbSunny />}
+                    />
+                </Flex>
             </Flex>
         </Flex>
     )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,61 @@
-import { Flex, Box, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { Flex, Box, Text, IconButton, Collapse, VStack, StackDivider } from "@chakra-ui/react";
 import { Link as ScrollLink } from "react-scroll";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
-const NavItem = ({ to, children }) => (
-    <ScrollLink to={to} smooth={true} duration={500} spy={true} offset={-50}>
+const NavItem = ({ to, onClick, children }) => (
+    <ScrollLink to={to} smooth={true} duration={500} spy={true} offset={-50} onClick={onClick}>
         <Box
-            mx={2}
+            m={1}
             cursor="pointer"
             borderBottom="2px solid transparent"
             _hover={{ borderBottomColor: "purple.600" }}
         >
-            <Text fontSize="xl">{children}</Text>
+            <Text fontSize="xl" color="black" >{children}</Text>
         </Box>
     </ScrollLink>
 );
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleNavItemClick = () => {
+        setIsOpen(false);
+    };
+
     return (
-        <Flex maxW="1200px" mx="auto" justifyContent="space-between" alignItems="center">
-            <Flex gap={5}>
+        <Flex position="relative" maxW="1200px" mx="auto" justifyContent="space-between" alignItems="center">
+            <IconButton
+                    icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                    aria-label="Toggle Navigation"
+                    onClick={() => setIsOpen(!isOpen)}
+                    display={{ md: "none" }} 
+                />
+            <Flex gap={5} display={{ base: "none", md: "flex" }} alignItems="center" >
                 <NavItem to="About">About</NavItem>
                 <Text fontSize="xl">|</Text>
                 <NavItem to="Skills">Skills</NavItem>
                 <Text fontSize="xl">|</Text>
                 <NavItem to="Projects">Projects</NavItem>
             </Flex>
+            <Collapse in={isOpen} animateOpacity>
+                <VStack
+                    py={2}
+                    spacing={2}
+                    align="center"
+                    bg="white"
+                    width={150}
+                    position="absolute"
+                    top="100%"
+                    right="0"
+                    zIndex="popover"
+                    divider={<StackDivider borderColor='gray.200' />}
+                    boxShadow="0 1px 3px rgba(0, 0, 0, 0.5)"
+                >
+                    <NavItem to="About" onClick={handleNavItemClick}>About</NavItem>
+                    <NavItem to="Skills" onClick={handleNavItemClick}>Skills</NavItem>
+                    <NavItem to="Projects" onClick={handleNavItemClick}>Projects</NavItem>
+                </VStack>
+            </Collapse>
         </Flex>
     );
 };
