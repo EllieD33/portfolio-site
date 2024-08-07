ad9cf7af2172be9fe06db02e97378417f3d0acdc
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-08-07 15:06:25 +0100

    refactors to reduce repetition and provide darkMode src for github icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,34 @@
-import { Flex, Image, Link, LinkBox, LinkOverlay, Text } from "@chakra-ui/react"
+import { Flex, Image, Link, LinkBox, LinkOverlay, Text, useColorMode } from "@chakra-ui/react"
 
 const Footer = () => {
+    const socialLinks = [
+        {
+            href: "https://github.com/EllieD33",
+            src: "https://img.icons8.com/glyph-neue/64/000000/github.png",
+            darkSrc: "https://img.icons8.com/glyph-neue/64/ffffff/github.png",
+            alt: "GitHub",
+            ariaLabel: "GitHub Profile"
+        },
+        {
+            href: "https://www.linkedin.com/in/ellen-daly-swe/",
+            src: "https://img.icons8.com/fluency/48/linkedin.png",
+            alt: "LinkedIn",
+            ariaLabel: "LinkedIn Profile"
+        }
+    ];
+
+    const { colorMode } = useColorMode();
+
     return (
         <Flex as="footer" flexDirection="column" alignItems="center" boxShadow="0 1px 3px rgba(0, 0, 0, 0.5)" >
-            <Flex mt={2} >
-                <LinkBox>
-                    <LinkOverlay href="https://github.com/EllieD33" isExternal aria-label="GitHub Profile">
-                        <Image
-                            src="https://img.icons8.com/fluency-systems-filled/48/github.png"
-                            alt="GitHub"
-                            boxSize="48px"
-                            objectFit="cover"                            
-                        />
-                    </LinkOverlay>
-                </LinkBox>
-                <LinkBox>
-                    <LinkOverlay href="https://www.linkedin.com/in/ellen-daly-swe/" isExternal aria-label="GitHub Profile">
-                        <Image
-                            src="https://img.icons8.com/fluency/48/linkedin.png"
-                            alt="LinkedIn"
-                            boxSize="48px"
-                            objectFit="cover"
-                        />
-                    </LinkOverlay>
-                </LinkBox>
+            <Flex mt={2} gap={4} >
+                {socialLinks.map(({ href, src, darkSrc, alt, ariaLabel }) => (
+                    <LinkBox key={href}>
+                        <LinkOverlay href={href} isExternal aria-label={ariaLabel}>
+                            <Image src={darkSrc && colorMode === 'dark' ? darkSrc : src} alt={alt} boxSize="48px" objectFit="cover" />
+                        </LinkOverlay>
+                    </LinkBox>
+                ))}
             </Flex>
             <Text mt={2} >Icons by
                 <Link href="https://icons8.com/" isExternal > Icons8</Link>
